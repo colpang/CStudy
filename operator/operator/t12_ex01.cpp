d13cb4799ed7647ec28928d25201dc3526fc6d67
jooyoung cha <ryuha.kuma@gmail.com>
2023-03-14 00:25:24 +0900

    12-13강
    if 조건문

---
+++ operator/operator/t12_ex01.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	if (n % 2 == 0) {
+		printf("nÀº Â¦¼ö\n");
+	}
+	else {
+		printf("nÀº È¦¼ö\n");
+	}
+}
---
+++ operator/operator/t12_ex02.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d",&n);
+
+	if (n>0) {
+		printf("n은 양수\n");
+	}
+	else if (n == 0) {
+		printf("n은 0\n");
+	}
+	else {
+		printf("n은 음수\n");
+	}
+}
